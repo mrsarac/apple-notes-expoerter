5d559d6f2a74db19014fdda8396a6e5ea98e3f1b
Mustafa <mrsarac@gmail.com>
2024-12-09 17:47:49 +0100

    Initial Commit

---
+++ apple-notes-expoerter/apple_notes_expoerterApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_notes_expoerterApp.swift
+//  apple-notes-expoerter
+//
+//  Created by Mustafa Saraç on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct apple_notes_expoerterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apple-notes-expoerterTests/apple_notes_expoerterTests.swift
@@ -0,0 +1,17 @@
+//
+//  apple_notes_expoerterTests.swift
+//  apple-notes-expoerterTests
+//
+//  Created by Mustafa Saraç on 9/12/24.
+//
+
+import Testing
+@testable import apple_notes_expoerter
+
+struct apple_notes_expoerterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
